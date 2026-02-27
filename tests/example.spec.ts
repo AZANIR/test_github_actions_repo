43911c0e539b54a5871e580a57a88fcb3ee989fd
AZANIR <AZANIR@users.noreply.github.com>
2026-02-27 19:09:04 +0000

    deploy: fdb126bee829b6cac2f8af39368c6238c16c3f16

--- tests/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('homepage has Playwright in title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/Playwright/);
-});
